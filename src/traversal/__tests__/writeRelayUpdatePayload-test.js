34de857fbaf70d162091bafb42514706dc168568
Huey Petersen <eyston@gmail.com>
2016-01-30 11:38:04 -0800

    fixed bad merge

--- src/traversal/__tests__/writeRelayUpdatePayload-test.js
+++ src/traversal/__tests__/writeRelayUpdatePayload-test.js
@@ -1041,7 +1041,7 @@ describe('writePayload()', () => {
           node: {
             id: nextNodeID,
             body: {
-              text: messageText,
+              text: input.message.text,
             },
           },
           source: {
@@ -1193,7 +1193,7 @@ describe('writePayload()', () => {
 
       writeRelayUpdatePayload(
         writer,
-        subscription,
+        mutation,
         payload,
         {configs, isOptimisticUpdate: false}
       );
@@ -1219,7 +1219,7 @@ describe('writePayload()', () => {
       expect(store.getField(nextNodeID, 'id')).toBe(nextNodeID);
       expect(store.getType(nextNodeID)).toBe('Comment');
       expect(store.getLinkedRecordID(nextNodeID, 'body')).toBe(bodyID);
-      expect(store.getField(bodyID, 'text')).toBe(messageText);
+      expect(store.getField(bodyID, 'text')).toBe(input.message.text);
       expect(store.getRangeMetadata(
         connectionID,
         [{name: 'first', value: '2'}]
@@ -1232,8 +1232,8 @@ describe('writePayload()', () => {
     it('non-optimistically prepends comments for subscriptions', () => {
       // create the subscription and payload
       var input = {
-        feedbackId: feedbackID,
         [RelayConnectionInterface.CLIENT_SUBSCRIPTION_ID]: '0',
+        feedbackId: feedbackID,
       };
 
       var subscription = getNode(Relay.QL`
@@ -1308,7 +1308,7 @@ describe('writePayload()', () => {
       );
 
       writeRelayUpdatePayload(
-        queryWriter,
+        writer,
         subscription,
         payload,
         {configs, isOptimisticUpdate: false}
